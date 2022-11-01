14b54e0c77e07285625b4c5d8ab08517faf572e4
Ravi Nagi <rasvindra@hotmail.com>
2022-11-01 15:20:16 -0700

    get request working

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,23 +2,26 @@ var searchInputEl = document.getElementById("userInput");
 var searchBtnEl = document.getElementById("searchBtn");
 var mainDisplayEl = document.getElementById("primary-card");
 var fivedayDisplayEl = document.getElementById("5-day-card");
+var searchData = [];
 
 searchBtnEl.addEventListener("click", userCity);
 
-var cityName = searchInputEl.value;
+
 
 function userCity (event) {
     event.preventDefault();
+    var cityName = searchInputEl.value;
     if (cityName === "") {
         alert ("No Name entered to Search");
         return;
     }
-    getData()
+
+    getAPIdata(cityName)
 }
 
-function getData(cityName) {
+function getAPIdata(cityName) {
     var APIKey = "5106b1dd029f01436cf1eff2fabc4fcf";
-    queryURL ="https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey + "&units=imperial";
+    queryURL ="https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey + "&units=imperial";
 
     fetch(queryURL)
         .then(function(response) {
@@ -34,8 +37,3 @@ function getData(cityName) {
 
         })
     }
-
-
-
-
-
