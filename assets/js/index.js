c641df3b880a91be3033743e5cf6b4a1403eaf70
Ravi Nagi <rasvindra@hotmail.com>
2022-11-01 15:07:16 -0700

    writing get/fetch function for user input

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,14 +5,37 @@ var fivedayDisplayEl = document.getElementById("5-day-card");
 
 searchBtnEl.addEventListener("click", userCity);
 
-
+var cityName = searchInputEl.value;
 
 function userCity (event) {
     event.preventDefault();
-    var cityName = searchInputEl.value;
     if (cityName === "") {
         alert ("No Name entered to Search");
         return;
     }
     getData()
 }
+
+function getData(cityName) {
+    var APIKey = "5106b1dd029f01436cf1eff2fabc4fcf";
+    queryURL ="https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey + "&units=imperial";
+
+    fetch(queryURL)
+        .then(function(response) {
+            if(!response.ok) {
+                console.log("error","tessssttttttttt 1111111111111111111")
+            } else {
+                return response.json();
+            }
+        })
+        .then(function(data){
+            console.log(data, "teeeeessssssssssttt 22222222222222222222")
+            displayData(data)
+
+        })
+    }
+
+
+
+
+
